66d4870ba3ec1d43318b2673c9524149ea37d797
ASC8 <asamcent@gmail.com>
2020-11-12 21:21:15 -0800

    sign up handlebar and js written

---
+++ public/javascript/sign-up.js
@@ -0,0 +1,43 @@
+async function signupFormHandler(event) {
+    event.preventDefault();
+
+    const email = document.querySelector('#email-signup').value.trim();
+    const username = document.querySelector('#username-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
+    const takenUsername = document.querySelector('#taken-email');
+    const takenUsername = document.querySelector('#taken-username');
+    
+    if (email && username && password) {
+        const response = await fetch('/api/users', {
+            method: 'post',
+            body: JSON.stringify({
+                email,
+                username,
+                password
+            }),
+            headers: { 'Content-Type': 'application/json' }
+        });
+            // get all users and compare username email
+            const getUsers = await fetch('/api/users').then(
+            function(response) {
+                response.json().then(function(data) {
+                    let ifTakenUsername = false;
+                    let ifTakenEmail = false; 
+                    for (let i = 0; i < data.length; i++) {
+                        if(username === data[i].username) {
+                            takenUsername.classList.remove('hide');
+                            return ifTakenUsername = true;
+                        } else if (email === data[i].email) {
+                            takenEmail.classList.remove('hide');
+                            return ifTakenEmail = true;
+                        }
+                    }
+                })
+            });
+        }  
+        if(response.ok) {
+            document.location.replace('/userinfo'); 
+    }
+}
+
+document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
