f04aef2819ee0e268095b4600da67b329895be91
ASC8 <asamcent@gmail.com>
2020-11-14 18:23:14 -0800

    write Create profile handlebar and front end JS

--- public/javascript/createUserProfile.js
+++ public/javascript/createUserProfile.js
@@ -1,3 +1,36 @@
+async function createProfileFormHandler(event) {
+    event.preventDefault();
+
+    const heightFeet = document.querySelector("#height-feet").value.trim();
+    const heightInches = document.querySelector("#height-inches").value.trim();
+    const height = heightFeet + "'" + heightInches + '"';
+    const fitnessLevel = document.querySelector("input[name='level']:checked").value;
+    const goals = Array
+        .from(document.querySelectorAll('input[type="checkbox"]'))
+        .filter((checkbox) => checkbox.checked)
+        .map((checkbox) => checkbox.value);
+
+    if (height && fitnessLevel && goals) {
+        const response = await fetch('/api/profile/:id', {
+            method: 'POST',
+            body: JSON.stringify({
+                height,
+                fitnessLevel,
+                goals
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        if (response.ok) {
+            document.location.replace('/dashboard')
+        } else {
+            console.log(response.statusText);
+        }
+    }
+}
+
 jQuery(function ($) {
     var checkList = $('.dropdown-check-list');
     checkList.on('click', 'span.anchor', function(event){
@@ -13,3 +46,5 @@ jQuery(function ($) {
         }
     });
 });
+
+document.querySelector('.user-info-form').addEventListener('submit', createProfileFormHandler);
